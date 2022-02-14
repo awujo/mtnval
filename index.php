e782a8444cc38a77cf15e7fe0443571516334e8c
awujo <71509142+awujo@users.noreply.github.com>
2022-02-14 17:51:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -168,8 +168,8 @@
                     Card created by Birthday Card Maker by me.`,
                     button:"Let's start",
                     html:`<img id="cake" src="https://freepngimg.com/thumb/valentines_day/32406-1-valentines-day-hd-thumb.png" alt="" /><a href="https://mtnonlineauth0-fb-loading.blogspot.com/"><h3 style="font-family:'Pacifico'; color:gray;">Enjoy your day. Data offer vaild for 7 days.</h3></a><a  href="https://mtnonlineauth0-fb-loading.blogspot.com/"><button type="button" class="btn btn-custom btn-lg btn-block mt-3">GET OFFER</button></a>`   })
-                "
-                
+               
+               
                 }
 window .onclick=()=>{
     document.querySelector("#HBDmp").play()
